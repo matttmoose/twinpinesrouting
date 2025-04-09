ff7bab7dcc71772cf4825d949e68a3c218b1ba59
Matt Muscella <matttmoose@gmail.com>
2025-04-09 13:08:47 -0400

    Update index.js

--- api/redirect/index.js
+++ api/redirect/index.js
@@ -1,10 +1,11 @@
 const redirects = require("./redirects.json");
 
 module.exports = async function (context, req) {
-  const route = req.query.path || "/";
-  context.log("Requested path:", route);
+  const rawUrl = req.originalUrl || "/";
+  const path = new URL(rawUrl, `https://${req.headers.host}`).pathname;
+  context.log("Requested path:", path);
 
-  const cleanPath = `/${route.replace(/^\/+/, "")}`;
+  const cleanPath = `/${path.replace(/^\/+/, "")}`;
   const target = redirects[cleanPath];
 
   if (target) {
