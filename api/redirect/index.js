b77ce2388d9ecd9f373d20c4208dc5052bc22179
Matt Muscella <matttmoose@gmail.com>
2025-04-09 13:32:16 -0400

    index.js with header

--- api/redirect/index.js
+++ api/redirect/index.js
@@ -1,8 +1,13 @@
 const redirects = require("./redirects.json");
 
 module.exports = async function (context, req) {
-  const raw = req.params.path || "/";
-  const cleanPath = `/${raw.replace(/^\/+/, "")}`;
+  const originalUrl = req.headers["x-ms-original-url"];
+  const path = new URL(originalUrl).pathname;
+
+  // Remove leading slashes and "/api/redirect" if present
+  const cleaned = path.replace(/^\/+/, "").replace(/^api\/redirect\/?/, "");
+  const cleanPath = `/${cleaned}`;
+
   context.log("Requested path:", cleanPath);
 
   const target = redirects[cleanPath];
